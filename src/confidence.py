ef2153391c1f278a8236a59aa54a3d6480f0b972
emmanuella <tekumnjit@gmail.com>
2023-12-11 10:28:16 +0100

    impimented confidence algorithm

--- src/confidence.py
+++ src/confidence.py
@@ -6,4 +6,36 @@ def confidence(data_set: dataset_type, rule: rule_type) -> float:
     """
     To measure the likelihood of occurrence of an itemset given another itemset.
     """
-    pass
+    # Mock dataset
+def calculate_support(dataset, itemset):
+    count = 0
+    for transaction in dataset:
+        if set(itemset).issubset(transaction):
+            count += 1
+    return count
+
+def calculate_confidence(dataset, rule):
+    antecedent = rule[0]
+    consequent = rule[1]
+    
+    support_XY = calculate_support(dataset, antecedent + consequent)
+    support_X = calculate_support(dataset, antecedent)
+    
+    if support_X != 0:
+        confidence = support_XY / support_X
+    else:
+        confidence = 0
+    
+    return confidence
+
+dataset = [
+    ["oil", "fish", "yam"],
+    ["oil", "fish"],
+    ["oil", "yam"],
+    ["fish", "yam"],
+    ["fish"]
+]
+rule = (["oil", "fish"], ["yam"])
+
+confidence = calculate_confidence(dataset, rule)
+print("Confidence:", confidence)
--- src/support.py
+++ src/support.py
@@ -6,4 +6,16 @@ def support(itemsets: dataset_type, data_set: itemset_type) -> float:
     """
     To find the frequency of itemsets in the dataset.
     """
-    pass
+
+        # Mock dataset
+dataset = [
+            ['top', 'milk', 'home'],
+            ['top', 'fanta', 'beer', 'fish'],
+            ['milk', 'fanta', 'beer', 'cola'],
+            ['top', 'milk', 'fanta', 'beer'],
+            ['top', 'milk', 'fanta', 'cola']
+        ]
+itemset = ['top', 'milk']
+
+support_value = support(itemset, dataset)
+print("Support value for itemset:", support_value)
