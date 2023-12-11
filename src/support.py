bf4eff7c44bb523907d8ac58f98b32d58e0c98df
emmanuella <tekumnjit@gmail.com>
2023-12-11 11:11:39 +0100

    Modified: I modified the support.py file

--- src/support.py
+++ src/support.py
@@ -6,16 +6,23 @@ def support(itemsets: dataset_type, data_set: itemset_type) -> float:
     """
     To find the frequency of itemsets in the dataset.
     """
-
+    count=0
+    for transaction in dataset:
+        if set(itemset).issubset(transaction):
+             count += 1
+    support_value=count/len(dataset)
+    return support_value
+ 
+if __name__==('__main__'):        
         # Mock dataset
-dataset = [
-            ['top', 'milk', 'home'],
-            ['top', 'fanta', 'beer', 'fish'],
-            ['milk', 'fanta', 'beer', 'cola'],
-            ['top', 'milk', 'fanta', 'beer'],
-            ['top', 'milk', 'fanta', 'cola']
-        ]
-itemset = ['top', 'milk']
+    dataset = [
+                ['top', 'milk', 'home'],
+                ['top', 'fanta', 'beer', 'fish'],
+                ['milk', 'fanta', 'beer', 'cola'],
+                ['top', 'milk', 'fanta', 'beer'],
+                ['top', 'milk', 'fanta', 'cola']
+            ]
+    itemset = ['milk']
 
-support_value = support(itemset, dataset)
-print("Support value for itemset:", support_value)
+    support_value = support(itemset, dataset)
+    print("Support value for itemset:", support_value)
